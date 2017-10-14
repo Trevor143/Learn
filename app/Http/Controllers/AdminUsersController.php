9fcb81b3141d5a019b6c0e4dbfdbdbb90aefedd8
Trevor143 <mukws18@gmail.com>
2017-10-14 22:47:37 +0300

    Users create form with user  pictures

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\Role;
 use App\User;
 use Illuminate\Http\Request;
@@ -47,8 +48,28 @@ class AdminUsersController extends Controller
     public function store(UsersRequest $request)
     {
         //
-        User::create($request->all());
-        return redirect('admin/users');
+
+        $input =$request->all();
+
+        $input['password']=bcrypt($request->password);
+
+        //persisting photo to database
+
+        if ($file = $request->file('photo_id')){
+            $name=time().$file->getClientOriginalName();
+
+            //moves specified file to folder in public directory
+            $file->move('images', $name);
+            $photo = Photo::create(['file' =>$name]);
+
+            $input['photo_id']=$photo->id;
+        }
+
+
+        User::create($input);
+
+//        User::create($request->all());
+                return redirect('admin/users');
 
 //        return $request->all();
     }
